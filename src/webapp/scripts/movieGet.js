5a6f9c0041a11845c72b5f8cfbeab282b0f3c7e2
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-08-31 16:43:06 +0000

    Finished adding get movie by title and added get methods for all fields

--- src/webapp/scripts/movieGet.js
+++ src/webapp/scripts/movieGet.js
@@ -1,41 +1,48 @@
 'use strict'
 
 let apiKey = "c00a3e5661629abbe2531b560b75a9cc";
+let movObj;
 
 
-class Movie {
-
- constructor(title){
-     this.title = title;
 
 
- }
-
-
-     getMovByTitle(title){
-         console.log("");
-         let movObj;
-         let req = new XMLHttpRequest();
-         console.log("https://api.themoviedb.org/3/search/movie?api_key="+ apiKey + "&query=" + title);
-         req.open("GET", "https://api.themoviedb.org/3/search/movie?api_key="+ apiKey + "&query=" + title);
-         req.responseType = "json";
-         req.send();
-         req.onload = function(){
-             movObj = req.response;
-             console.log(movObj);
-
-         }
-
+let getMovByTitle = (title) => {
+    let req = new XMLHttpRequest();
+    req.open("GET", "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&query=" + title);
+    req.responseType = "json";
+    req.send();
+    req.onload = function () {
+        movObj = req.response;
+        return movObj;
     }
+}
 
+let getTitle = () => {
+    return movObj.results[0].title;
+}
 
+let getOverview = () => {
+    return movObj.results[0].overview;
+}
+
+let getReleaseDate = () => {
+    return movObj.results[0].release_date;
+}
+
+let getPosterPath = (size) => {
+    return "https://image.tmdb.org/t/p/" + size + movObj.results[0].poster_path;
+}
+
+let getBackDropPath = (size) => {
+    return "https://image.tmdb.org/t/p/" + size + movObj.results[0].backdrop_path;
+}
+
+let getLang = () => {
+    return movObj.results[0].original_language;
+}
+
+let getPopularity = () => {
+    return Math.round(movObj.results[0].vote_average);
 }
 
 
-function tester(){
-  let mg = new Movie("transformers");
-
-  mg.getMovByTitle("transformers");
-
-
-}
