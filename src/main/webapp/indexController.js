30329ce657155bf9859c5bf0f18a8239c2e5fdb4
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-09-13 13:38:06 +0000

    index updates

--- src/main/webapp/indexController.js
+++ src/main/webapp/indexController.js
@@ -1,10 +1,16 @@
 (function() {
 
-    var indexController =  function() {
+    var indexController =  function($state) {
         var vm = this;
 
-        
+        vm.searchBar = function () {
+
+
+                    console.log("enter");
+                    $state.go('search');
+
+        };
     };
 
-    angular.module('apolloCinema').controller('indexController', [indexController]);
+    angular.module('apolloCinema').controller('indexController', ['$state' , indexController]);
 }());
