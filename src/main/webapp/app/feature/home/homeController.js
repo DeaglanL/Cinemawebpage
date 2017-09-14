82a69ad15991ec602e422c0881be1b2612230e2e
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-09-14 17:29:11 +0000

    added ability to get pictures dynamically to home controller

--- src/main/webapp/app/feature/home/homeController.js
+++ src/main/webapp/app/feature/home/homeController.js
@@ -1,8 +1,17 @@
 (function() {
+    var HomeController =  function(apiGet) {
 
-    var HomeController =  function() {
+        var vm = this;
+
+
+        vm.getPic = function () {
+            apiGet.getMovie("Transformers").then(function (result) {
+                vm.Src =  "https://image.tmdb.org/t/p/w500" + result.results[0].backdrop_path;
+            })
+        }
+
+        vm.getPic();
 
     };
-
-    angular.module('apolloCinema').controller('HomeController', [HomeController]);
+    angular.module("apolloCinema").controller("HomeController", ["apiGet", HomeController]);
 }());
