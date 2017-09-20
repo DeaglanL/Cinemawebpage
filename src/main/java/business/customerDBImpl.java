e74cbec59b550db6c815a6388a40a40d5c88d891
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-09-20 15:37:47 +0000

    added constructors

--- src/main/java/business/customerDBImpl.java
+++ src/main/java/business/customerDBImpl.java
@@ -1,7 +1,47 @@
 package business;
 
+import persistance.CustomerTableController;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+
+@Default
+@Stateless
 public class customerDBImpl implements customerService {
+
+    private String ip;
+    private String port;
+    private String dbName;
+    private String username;
+    private String password;
+
+    @Inject
+    CustomerTableController custControl;
+
+    public customerDBImpl(){
+         ip = "sql11.freemysqlhosting.net";
+         port = "3306";
+         dbName = "sql11195115";
+         username = "sql11195115";
+         password = "p21IgmB3mn";
+    }
+
+    public customerDBImpl(String ip,String port,String dbName, String username,String password){
+        this.ip = ip;
+        this.port = port;
+        this.dbName = dbName;
+        this.username = username;
+        this.password = password;
+    }
+
     public String addCustomer(String jsonCustomer) {
+
+        custControl.createConnection(ip, port, dbName, username, password);
+
+
+
+
         return null;
     }
 
