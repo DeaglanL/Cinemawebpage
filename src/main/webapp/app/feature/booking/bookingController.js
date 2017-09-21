6dbdbe361f82e93e179e6c51b03ac54da0e2375c
AlexMacleod23 <amacleod231@gmail.com>
2017-09-21 11:49:30 +0100

    Finished creating booking page, controller not finished

--- src/main/webapp/app/feature/booking/bookingController.js
+++ src/main/webapp/app/feature/booking/bookingController.js
@@ -6,20 +6,23 @@
         let vm = this;
 
         vm.movieTitle = "Movie Title";
-        vm.moviePosterThumbnail = "poster path";
-        vm.movieLength = "Movie duration";
+        vm.moviePosterThumbnail = "";
+        vm.movieLength = "##mins";
         vm.cinema = "Cinema";
         vm.screenName = "Screen ##";
-        vm.screeningDate = "Date of screening";
-        vm.screeningTime = "Time of screening";
+        vm.screeningDate = "##/##/####";
+        vm.screeningTime = "##:##";
 
         vm.numAdults = 0;
         vm.numConcessions = 0;
-        vm.ticketPriceAdult = 10;
-        vm.ticketPriceConcession = 7.50;
+        vm.ticketPriceAdult = "10.00";
+        vm.ticketPriceConcession = "7.50";
 
         vm.submitTicketDetails = function () {
-
+            let ticket = {"adults":vm.numAdults,
+                        "concessions":vm.numConcessions,
+                        "screeningID":"",
+                        "seatID":""};
         }
     };
 
