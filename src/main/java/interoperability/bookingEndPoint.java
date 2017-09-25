4c153833761befaf319bca8da6da1792fca6e60c
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-09-25 13:54:03 +0000

    gits being weird

--- src/main/java/interoperability/bookingEndPoint.java
+++ src/main/java/interoperability/bookingEndPoint.java
@@ -1,4 +1,30 @@
 package interoperability;
 
+import business.BookingService;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/booking")
 public class bookingEndPoint {
+
+@Inject
+    BookingService bs;
+
+    @POST
+    @Path("/json")
+    @Produces({ "application/json" })
+    public String checkCustomer(String inputJSON) {
+        return "null";
+    }
+
+    @PUT
+    @Path("/json")
+    @Produces({ "application/json" })
+    public String addNewCustomer(String inputJSON) {
+        return "null";
+    }
 }
