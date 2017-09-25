d0730051396b9ed79846c612d1160f95bf0f51a2
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-09-25 17:27:34 +0000

    tdd

--- src/main/java/business/BookingService.java
+++ src/main/java/business/BookingService.java
@@ -1,4 +1,6 @@
 package business;
 
 public interface BookingService {
+    String getCinemaInfo(String cinema);
+    String getScreenings(String movie);
 }
--- src/test/java/backendTests/bookingDBImplTests.java
+++ src/test/java/backendTests/bookingDBImplTests.java
@@ -1,10 +1,13 @@
 package backendTests;
 
+import business.bookingDBImpl;
 import org.junit.*;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class bookingDBImplTests {
+
+    private bookingDBImpl bookService;
 
     @BeforeClass
     public static void beforeClass() {
@@ -12,12 +15,38 @@ public class bookingDBImplTests {
 
     @Before
     public void beforeTest() {
+        bookService = new bookingDBImpl();
 
     }
 
     @Test
-    public void test(){
+    public void getCinemaInfoTest(){
+        assertEquals("should return json cinema inf0", "{\n" +
+                "  \"cinemaid\" : \"Some text\",\n" +
+                "  \"address\" : \"Some text\",\n" +
+                "  \"openingTime\" : \"Some text\",\n" +
+                "  \"closingTime\" : \"Some text\",\n" +
+                "  \"name\" : \"Some text\",\n" +
+                "  \"numberOfScreens\" : \"Some text\",\n" +
+                "  \"cinemascol\" : \"Some text\",\n" +
+                "  \"screens_screensid\" : \"Some text\"\n" +
+                "} \n" , bookService.getCinemaInfo("{\"name\" : \"Some text\"}"));
+    }
 
+    @Test
+    public void getScreeningsTest(){
+       assertEquals("should return screenings objects", "{\n" +
+               "\t\"screening\"  : \"Some Text\",\n" +
+               "\t\"screeningid\" : \"Some Text\",\n" +
+               "\t\"cinema\" : \"Some Text\",\n" +
+               "\t\"screenid\" : \"Some Text\",\n" +
+               "\t\"time\" : \"Some Text\",\n" +
+               "\t\"movieid\" : \"Some Text\",\n" +
+               "\t\"date\" : \"Some Text\",\n" +
+               "\t\"movies_movieid\" : \"Some Text\",\n" +
+               "\t\"movies_customers_customersid\" : \"Some Text\",\n" +
+               "\t\"screens_screensid\" : \"Some Text\"\n" +
+               "}", bookService.getScreenings("{\"cinemaName\" : \"Some text \", \"moviename\" : \"Some text\"}"));
     }
 
 
