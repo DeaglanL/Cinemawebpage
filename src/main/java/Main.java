8c22f76fcc19cccad98a02e845a6aa8ce1f7f8f5
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-09-25 15:38:55 +0000

    removed unused code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,5 @@ import java.sql.Connection;
 public class Main {
 
     public static void main(String[] args) {
-
-/*
-        TicketTableController x = new TicketTableController();
-
-
-        persistance.MasterController y = new persistance.MasterController();
-        Connection conn = y.createConnection("46.32.240.39 ", "3306", "apoll-ila-u-141465", "apoll-ila-u-141465", "gMgN/yVq3");
-
-        System.out.println(x.getMovieById(1, conn).getTitle());
-
-    */
-
     }
 }
