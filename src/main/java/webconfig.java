16c8de4facb281026760f0cf36438d64a123b049
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-09-26 09:46:43 +0000

    got website to deploy via spring boot but resful services are broken

---
+++ src/main/java/webconfig.java
@@ -0,0 +1,17 @@
+package com.qa.springboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class webconfig extends WebMvcConfigurerAdapter {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+}
