f6af09ed9da4766a9f7ec49aad28a6596ca46955
bkaremba <bkaremba13@gmail.com>
2017-09-26 10:53:11 +0100

    screen and products completed, screening is in progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,22 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        TicketTableController x = new TicketTableController();
-
-
         MasterController y = new MasterController();
         Connection conn = y.createConnection("46.32.240.39 ", "3306", "apoll-ila-u-141465", "apoll-ila-u-141465", "gMgN/yVq3");
-
-        System.out.println(x.getMovieById(1, conn).getTitle());
-
-
-
-
-
-
-
-
 
     }
 
