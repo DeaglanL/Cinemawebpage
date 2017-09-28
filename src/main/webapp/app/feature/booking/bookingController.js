842ffd04b75ea809e7afe5e3d345f1b52227a46a
David Jiang <david.jiang@qa.com>
2017-09-28 09:12:18 +0100

    removed all logs from file

--- src/main/webapp/app/feature/booking/bookingController.js
+++ src/main/webapp/app/feature/booking/bookingController.js
@@ -6,8 +6,7 @@
         let vm = this;
         let sharedMovieInfo = $rootScope.sharedMovie;
         let sharedMovieInfo2 = $rootScope.sharedMovie2;
-
-        console.log(sharedMovieInfo);
+        
 
         vm.movieTitle = sharedMovieInfo.results[0].title;
         vm.moviePosterThumbnail = "https://image.tmdb.org/t/p/w500" + sharedMovieInfo.results[0].poster_path;
@@ -19,19 +18,61 @@
 
         //vm.movie = $rootScope.sharedMovie1 + ;
 
-        vm.screenings = [{"screeningID":41,"cinema":"London","screenID":1,"time":"14:55","movieID":3,"date":"02/10/17"},
-                        {"screeningID":42,"cinema":"London","screenID":4,"time":"16:20","movieID":3,"date":"02/10/17"},
-                        {"screeningID":43,"cinema":"Manchester","screenID":2,"time":"17:10","movieID":3,"date":"02/10/17"},
-                        {"screeningID":44,"cinema":"Sheffield","screenID":1,"time":"19:50","movieID":3,"date":"02/10/17"},
-                        {"screeningID":45,"cinema":"Manchester","screenID":5,"time":"19:50","movieID":3,"date":"02/10/17"},
-                        {"screeningID":46,"cinema":"London","screenID":4,"time":"20:25","movieID":3,"date":"02/10/17"},
-                        {"screeningID":47,"cinema":"Sheffield","screenID":3,"time":"21:30","movieID":3, "date":"02/10/17"},
-                        {"screeningID":48,"cinema":"Manchester","screenID":2,"time":"23:00","movieID":3,"date":"02/10/17"}];
+        vm.screenings = [{
+            "screeningID": 41,
+            "cinema": "London",
+            "screenID": 1,
+            "time": "14:55",
+            "movieID": 3,
+            "date": "02/10/17"
+        },
+            {"screeningID": 42, "cinema": "London", "screenID": 4, "time": "16:20", "movieID": 3, "date": "02/10/17"},
+            {
+                "screeningID": 43,
+                "cinema": "Manchester",
+                "screenID": 2,
+                "time": "17:10",
+                "movieID": 3,
+                "date": "02/10/17"
+            },
+            {
+                "screeningID": 44,
+                "cinema": "Sheffield",
+                "screenID": 1,
+                "time": "19:50",
+                "movieID": 3,
+                "date": "02/10/17"
+            },
+            {
+                "screeningID": 45,
+                "cinema": "Manchester",
+                "screenID": 5,
+                "time": "19:50",
+                "movieID": 3,
+                "date": "02/10/17"
+            },
+            {"screeningID": 46, "cinema": "London", "screenID": 4, "time": "20:25", "movieID": 3, "date": "02/10/17"},
+            {
+                "screeningID": 47,
+                "cinema": "Sheffield",
+                "screenID": 3,
+                "time": "21:30",
+                "movieID": 3,
+                "date": "02/10/17"
+            },
+            {
+                "screeningID": 48,
+                "cinema": "Manchester",
+                "screenID": 2,
+                "time": "23:00",
+                "movieID": 3,
+                "date": "02/10/17"
+            }];
 
         vm.ticketPriceAdult = 10;
         vm.ticketPriceConcession = 7.5;
 
-        vm.choose = function(screening) {
+        vm.choose = function (screening) {
             vm.screen = screening.screenID;
             vm.screeningDate = screening.date;
             vm.screeningTime = screening.time;
